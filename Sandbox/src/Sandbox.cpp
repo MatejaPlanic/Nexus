18baef4e7793c1984f88c72ef35aa39d2dcba78d
Mateja Planic <matejaplanic123@gmail.com>
2026-04-06 01:38:00 +0200

    Stting everything up, basic dll linking with sandbox and nexus projects, also made an entrypoint

---
+++ Sandbox/src/Sandbox.cpp
@@ -0,0 +1,19 @@
+#include "Nexus.h"
+
+class Sandbox : public Nexus::Application
+{
+public:
+	Sandbox()
+	{
+
+	}
+	~Sandbox()
+	{
+
+	}
+};
+
+Nexus::Application* Nexus::CreateApplication()
+{
+	return new Sandbox();
+}
