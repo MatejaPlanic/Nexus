d9357ca60b41f3114f34d97e19a3b47184bab5d3
Mateja Planic <matejaplanic123@gmail.com>
2026-04-09 23:28:53 +0200

    Implemented Window class, specifically for Windows platform, also added new gitmodule GLFW.

--- Nexus/src/Nexus/Application.cpp
+++ Nexus/src/Nexus/Application.cpp
@@ -4,20 +4,24 @@
 #include "Nexus/Events/ApplicationEvent.h"
 #include "Nexus/Log.h"
 
+#include <GLFW/glfw3.h>
+
 namespace Nexus
 {
 	Application::Application()
 	{
+		m_Window = std::unique_ptr<Window>(Window::Create());
 	}
 	Application::~Application()
 	{
 	}
 	void Application::Run()
 	{
-		WindowResizeEvent e(1280, 720);
-
-		NX_TRACE(e.ToString());
-
-		while (true);
+		while (m_Running)
+		{
+			glClearColor(0.1f, 0.1f, 0.1f, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
+		}
 	}
 }
