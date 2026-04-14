77539504b71639e113325f31bbb6566a07fd02c0
Mateja Planic <matejaplanic123@gmail.com>
2026-04-15 00:48:00 +0200

    Registered glfwevent callbacks and tested current event system

--- Nexus/src/Nexus/Application.h
+++ Nexus/src/Nexus/Application.h
@@ -3,6 +3,7 @@
 #include "Core.h"
 #include "Window.h"
 #include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
 
 namespace Nexus
 {
@@ -14,7 +15,10 @@ namespace Nexus
 
 		void Run();
 
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
