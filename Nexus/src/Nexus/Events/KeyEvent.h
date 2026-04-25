653c02806fc071baa92fca0dcffce9af15e005ca
Mateja Planic <matejaplanic123@gmail.com>
2026-04-25 02:00:56 +0200

    Implemented basic events for imgui

--- Nexus/src/Nexus/Events/KeyEvent.h
+++ Nexus/src/Nexus/Events/KeyEvent.h
@@ -48,4 +48,21 @@ namespace Nexus {
 		EVENT_CLASS_TYPE(KeyReleased)
 	};
 
+	class NEXUS_API KeyTypedEvent : public KeyEvent
+	{
+	public:
+		KeyTypedEvent(int keycode) : KeyEvent(keycode) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyTypedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyTyped)
+	private:
+		int m_RepeatCount;
+	};
+
 }
